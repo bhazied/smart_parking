f63c9540c8edb14b629d92ac369210f661636b64
Zied Ben Hadj Amor <zied.benhadjamor@continuousnet.com>
2017-05-23 18:53:17 +0100

    update security example

--- config/security.php
+++ config/security.php
@@ -9,6 +9,11 @@ return [
         ['uri' => '^api/users/{user}$', 'method' => ['get', 'head'], 'roles' => ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']],
         ['uri' => '^api/users/{user}$', 'method' => ['put', 'patch'], 'roles' => ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']],
         ['uri' => '^api/users/{user}$', 'method' => ['delete'], 'roles' => ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']],
-        //Car Access Controle
+        //Country Access Controle
+        ['uri' => '^api/countries$', 'method' => ['get', 'head'], 'roles' => ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']],
+        ['uri' => '^api/countries$', 'method' => ['post'], 'roles' => ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']],
+        ['uri' => '^api/countries$/{user}$', 'method' => ['get', 'head'], 'roles' => ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']],
+        ['uri' => '^api/countries$/{user}$', 'method' => ['put', 'patch'], 'roles' => ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']],
+        ['uri' => '^api/countries$/{user}$', 'method' => ['delete'], 'roles' => ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']],
     ]
 ];
--- routes/api.php
+++ routes/api.php
@@ -27,16 +27,23 @@ Route::group(['middleware' => ['auth:api', 'autrhorization']], function () {
         'roles' => ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']
     ]);
     Route::resource('users', 'Api\UserController');
-    Route::resource('cars', 'Api\CarController', ['only' => 'create', 'edit', 'delete']);
+    Route::resource('cars', 'Api\CarController');
+    Route::resource('countries', 'Api\CountryController');
+    Route::resource('states', 'Api\StateController');
+    Route::resource('regions', 'Api\RegionController');
+    Route::resource('languages', 'Api\LanguageController');
+    Route::resource('parkings', 'Api\ParkingController');
+    Route::resource('car_bodies', 'Api\CarBodyController');
+    Route::resource('car_models', 'Api\CarModelController');
+    Route::resource('car_brands', 'Api\CarBrandController');
 });
 
 Route::post('/login', 'Auth\ApiLoginController@login');
-Route::resource('cars', 'Api\CarController', ['only' => 'index']);
-Route::resource('countries', 'Api\CountryController', ['except' => ['create', 'edit']]);
-Route::resource('states', 'Api\StateController', ['except' => ['create', 'edit']]);
-Route::resource('regions', 'Api\RegionController', ['except' => ['create', 'edit']]);
-Route::resource('languages', 'Api\LanguageController', ['except' => ['create', 'edit']]);
-Route::resource('parkings', 'Api\ParkingController', ['except' => ['create', 'edit']]);
-Route::resource('car_bodies', 'Api\CarBodyController', ['except' => ['create', 'edit']]);
-Route::resource('car_models', 'Api\CarModelController', ['except' => ['create', 'edit']]);
-Route::resource('car_brands', 'Api\CarBrandController', ['except' => ['create', 'edit']]);
+Route::resource('countries', 'Api\CountryController', ['only' => 'index']);
+Route::resource('states', 'Api\StateController', ['only' => 'index']);
+Route::resource('regions', 'Api\RegionController', ['only' => 'index']);
+Route::resource('languages', 'Api\LanguageController', ['only' => 'index']);
+Route::resource('parkings', 'Api\ParkingController', ['only' => 'index']);
+Route::resource('car_bodies', 'Api\CarBodyController', ['only' => 'index']);
+Route::resource('car_models', 'Api\CarModelController', ['only' => 'index']);
+Route::resource('car_brands', 'Api\CarBrandController', ['only' => 'index']);
